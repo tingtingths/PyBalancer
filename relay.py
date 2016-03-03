78c03765b8b0b02c58b6dc009cb135a2ec05ba4c
Tony Ting <k999hoshing@gmail.com>
2016-03-03 16:56:40 +0800

    Every relay operation is in a new thread. Add backend priority/rating.

--- relay.py
+++ relay.py
@@ -3,12 +3,26 @@ import threading
 import sys
 import datetime
 
+id_pool = list(range(0, 10000))
+pending_poll = []
 
-def pipe(source, dest):
+def notify_end(id, source, dest):
+    lock = threading.Lock()
+    lock.acquire()
+    if id in pending_poll:
+        source.close()
+        dest.close()
+        pending_poll.remove(id)
+        id_pool.append(id)
+    else:
+        pending_poll.append(id)
+    lock.release()
+
+def pipe(id, source, dest):
     source.settimeout(2)
     while True:
         try:
-            data = source.recv(4194304)
+            data = source.recv(10485760)
             if not data: break
             dest.send(data)
             time = datetime.datetime.today().strftime("[%d/%b/%Y %H:%M:%S]")
@@ -19,38 +33,58 @@ def pipe(source, dest):
             print(e)
             break
     dest.shutdown(socket.SHUT_WR)
+    notify_end(id, source, dest)
 
-def relay(port, backend_hosts):
-    listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listen_sock.bind(("", port))
-    listen_sock.listen(64)
-    listen_sock.settimeout(4)
-    rr_ptr = 0 # idx of pervious backend server used in round-robin style
+def setup_pipes(r_conn, target):
+    global id_pool
 
-    while True:
-        try:
-            # round-robin
-            if len(backend_hosts) - 1 <= rr_ptr:
-                rr_ptr = 0
-            else:
-                rr_ptr += 1
+    print("target: " + str(target))
+    backend = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    backend.connect(target)
+    id = id_pool.pop(0)
 
-            r_conn, r_addr = listen_sock.accept()
-            backend = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            backend.connect(backend_hosts[rr_ptr])
-            print("Connected from " + r_addr[0])
+    threading.Thread(target=pipe, args=(id, r_conn, backend)).start()
+    threading.Thread(target=pipe, args=(id, backend, r_conn)).start()
 
-            threading.Thread(target=pipe, args=(r_conn, backend)).start()
-            threading.Thread(target=pipe, args=(backend, r_conn)).start()
+class Relay(threading.Thread):
+    def __init__(self, port, backend_hosts):
+        super().__init__()
+        self.port = port
+        self.backend_hosts = backend_hosts
 
-            #handle(r_conn, r_addr, backend_sock)
-        except socket.timeout:
-            pass
-        except Exception as e:
-            print(e)
-        except KeyboardInterrupt:
-            pass
+    def run(self):
+        print("Relay " +str(self.port))
+        listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listen_sock.bind(("localhost", self.port))
+        listen_sock.listen(512)
+        listen_sock.settimeout(4)
+        rr_ptr = -1 # idx of pervious backend server used in round-robin style
+
+        while True:
+            try:
+                r_conn, r_addr = listen_sock.accept()
+                print("Connect from " + r_addr[0])
+
+                get_host = True
+                while get_host:
+                    # round-robin
+                    if len(self.backend_hosts) - 1 <= rr_ptr:
+                        rr_ptr = 0
+                    else:
+                        rr_ptr += 1
+                    if self.backend_hosts[rr_ptr][-1] >= 0:
+                        get_host = False
+
+                threading.Thread(target=setup_pipes, args=(r_conn, self.backend_hosts[rr_ptr][0] )).start()
+                #r_conn.close()
+            except socket.timeout:
+                pass
+            except Exception as e:
+                pass
+            except KeyboardInterrupt:
+                pass
 
 if __name__ == "__main__":
-    backend = [("localhost", 4343)]
-    relay(80, backend)
+    # ((addr, port), priority)
+    backend = [(("localhost", 4343), -1), (("ting-xps", 4343), 0), (("ting-xps", 4545), 0)]
+    Relay(54321, backend).start()
