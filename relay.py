58d419fbb55d08c8b8ebb097222fb02d77fc73a9
Tony Ting <k999hoshing@gmail.com>
2016-10-24 21:06:21 +0800

    Remove unnecessary function. Format code.

--- relay.py
+++ relay.py
@@ -3,34 +3,10 @@ import threading
 import sys
 import datetime
 
-id_pool = list(range(0, 10000))
-pending_close = []
-to_print = True
+DEBUG = True
 
 
-def notify_end(id, source, dest):
-    """Prepare to close the socket,
-    or actually close it.
-    Each relay has two socket connections each handle by one thread,
-        client --> target and
-        target --> client.
-    The program close both connection if both threads are finished.
-    """
-    lock = threading.Lock()
-    lock.acquire()
-    if id in pending_close:
-        # the other thread is done, close all
-        source.close()
-        dest.close()
-        pending_close.remove(id)
-        id_pool.append(id)
-    else:
-        # the other thread not done, prepare to close this
-        pending_close.append(id)
-    lock.release()
-
-
-def pipe(id, source, dest):
+def pipe(source, dest):
     source.settimeout(2)
 
     while True:
@@ -39,31 +15,26 @@ def pipe(id, source, dest):
             if not data:
                 break
             dest.send(data)
-            if to_print:
+            if DEBUG:
                 time = datetime.datetime.today().strftime(
                     "[%d/%b/%Y %H:%M:%S]")
                 print(time + " " + str(source.getpeername()) + " --" +
                       str(len(data)) + " bytes--> " + str(dest.getpeername()))
-        except socket.timeout:
-            pass
-        except Exception as e:
-            print(e)
+        except:
             break
-
-    dest.shutdown(socket.SHUT_WR)
-    notify_end(id, source, dest)
+        finally:
+            source.close()
+            dest.close()
 
 
 def setup_pipes(r_conn, target):
-    global id_pool
-
-    print("target: " + str(target))
+    if DEBUG:
+        print("target: " + str(target))
     _target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     _target.connect(target)
-    id = id_pool.pop(0)
 
-    threading.Thread(target=pipe, args=(id, r_conn, _target)).start()
-    threading.Thread(target=pipe, args=(id, _target, r_conn)).start()
+    threading.Thread(target=pipe, args=(r_conn, _target)).start()
+    threading.Thread(target=pipe, args=(_target, r_conn)).start()
 
 
 class Relay(threading.Thread):
@@ -84,7 +55,8 @@ class Relay(threading.Thread):
         while True:
             try:
                 r_conn, r_addr = listen_sock.accept()
-                print("Connect from " + r_addr[0])
+                if DEBUG:
+                    print("Connect from " + r_addr[0])
 
                 get_host = True
                 while get_host:
@@ -96,18 +68,14 @@ class Relay(threading.Thread):
                     if self.target_hosts[rr_ptr][-1] >= 0:
                         get_host = False
 
-                threading.Thread(target=setup_pipes, args=(
-                    r_conn, self.target_hosts[rr_ptr][0])).start()
-            except socket.timeout:
+                threading.Thread(
+                    target=setup_pipes, args=(
+                        r_conn, self.target_hosts[rr_ptr][0])).start()
+            except:
                 pass
-            except KeyboardInterrupt:
-                pass
-            except Exception as e:
-                print(e)
 
 
 if __name__ == "__main__":
-    to_print = True
     # ((addr, port), priority)
     targets = [(("ipinfo.io", 80), 1)]
     Relay(54321, targets).start()
