99ea08d2bee08d1f200d1aaf2fe5a4713ed77c8d
Tony Ting <k999hoshing@gmail.com>
2016-10-24 14:22:26 +0800

    Add some comments and format code.

--- relay.py
+++ relay.py
@@ -4,61 +4,82 @@ import sys
 import datetime
 
 id_pool = list(range(0, 10000))
-pending_poll = []
+pending_close = []
+to_print = True
+
 
 def notify_end(id, source, dest):
+    """Prepare to close the socket,
+    or actually close it.
+    Each relay has two socket connections each handle by one thread,
+        client --> target and
+        target --> client.
+    The program close both connection if both threads are finished.
+    """
     lock = threading.Lock()
     lock.acquire()
-    if id in pending_poll:
+    if id in pending_close:
+        # the other thread is done, close all
         source.close()
         dest.close()
-        pending_poll.remove(id)
+        pending_close.remove(id)
         id_pool.append(id)
     else:
-        pending_poll.append(id)
+        # the other thread not done, prepare to close this
+        pending_close.append(id)
     lock.release()
+
 
 def pipe(id, source, dest):
     source.settimeout(2)
+
     while True:
         try:
             data = source.recv(10485760)
-            if not data: break
+            if not data:
+                break
             dest.send(data)
-            time = datetime.datetime.today().strftime("[%d/%b/%Y %H:%M:%S]")
-            print(time + " " + str(source.getpeername()) + " --" + str(len(data)) + " bytes--> " + str(dest.getpeername()))
+            if to_print:
+                time = datetime.datetime.today().strftime(
+                    "[%d/%b/%Y %H:%M:%S]")
+                print(time + " " + str(source.getpeername()) + " --" +
+                      str(len(data)) + " bytes--> " + str(dest.getpeername()))
         except socket.timeout:
             pass
         except Exception as e:
             print(e)
             break
+
     dest.shutdown(socket.SHUT_WR)
     notify_end(id, source, dest)
+
 
 def setup_pipes(r_conn, target):
     global id_pool
 
     print("target: " + str(target))
-    backend = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    backend.connect(target)
+    _target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    _target.connect(target)
     id = id_pool.pop(0)
 
-    threading.Thread(target=pipe, args=(id, r_conn, backend)).start()
-    threading.Thread(target=pipe, args=(id, backend, r_conn)).start()
+    threading.Thread(target=pipe, args=(id, r_conn, _target)).start()
+    threading.Thread(target=pipe, args=(id, _target, r_conn)).start()
+
 
 class Relay(threading.Thread):
-    def __init__(self, port, backend_hosts):
+
+    def __init__(self, port, target_hosts):
         super().__init__()
         self.port = port
-        self.backend_hosts = backend_hosts
+        self.target_hosts = target_hosts
 
     def run(self):
-        print("Relay " +str(self.port))
         listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listen_sock.bind(("localhost", self.port))
+        listen_sock.bind(("0.0.0.0", self.port))
         listen_sock.listen(512)
         listen_sock.settimeout(4)
-        rr_ptr = -1 # idx of pervious backend server used in round-robin style
+        rr_ptr = -1  # idx of pervious backend server used in round-robin style
+        print("Relay " + str(self.port))
 
         while True:
             try:
@@ -68,15 +89,16 @@ class Relay(threading.Thread):
                 get_host = True
                 while get_host:
                     # round-robin
-                    if len(self.backend_hosts) - 1 <= rr_ptr:
+                    if len(self.target_hosts) - 1 <= rr_ptr:
                         rr_ptr = 0
                     else:
                         rr_ptr += 1
-                    if self.backend_hosts[rr_ptr][-1] >= 0:
+                    if self.target_hosts[rr_ptr][-1] >= 0:
                         get_host = False
 
-                threading.Thread(target=setup_pipes, args=(r_conn, self.backend_hosts[rr_ptr][0] )).start()
-                #r_conn.close()
+                threading.Thread(target=setup_pipes, args=(
+                    r_conn, self.target_hosts[rr_ptr][0])).start()
+                # r_conn.close()
             except socket.timeout:
                 pass
             except Exception as e:
@@ -85,6 +107,7 @@ class Relay(threading.Thread):
                 pass
 
 if __name__ == "__main__":
+    to_print = True
     # ((addr, port), priority)
-    backend = [(("localhost", 4343), -1), (("ting-xps", 4343), 0), (("ting-xps", 4545), 0)]
-    Relay(54321, backend).start()
+    targets = [(("ipinfo.io", 80), 1)]
+    Relay(54321, targets).start()
